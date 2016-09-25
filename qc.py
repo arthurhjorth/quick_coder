ebfd2d150b357684ba6352804137c900eb46c2ab
Arthur Hjorth <arthur.hjorth@u.northwestern.edu>
2016-09-25 08:15:48 -0500

    refactored

--- qc.py
+++ qc.py
@@ -4,35 +4,24 @@ import sys
 infile = sys.argv[1]
 
 #  okay, now we have the file name. Open it up and figure out what the fieldnames are; just read the first line and split it by delimiter
-fieldnames = []
-with open(infile) as inf:
-    first_line = inf.readline()
-    fieldnames = first_line.split(",")# might want to prompt for custom delimeter
-fname_ids = zip(fieldnames, range(len(fieldnames)))
+
+def get_fieldnames():
+    with open(infile) as inf:
+        first_line = inf.readline()
+        return first_line.split(",")# might want to prompt for custom delimeter
+fieldnames = get_fieldnames()
+fname_ids = [d for d in zip(range(len(fieldnames)), fieldnames)]
 
 # now ask user which ones to code:
-print("coding "  + str(infile.split("/")[-1:]))
-print("which fields do you want to code?")
-for fname in fname_ids:
-    print(str(fname[1]) + " : " + str(fname[0]))
-
-fieldnames_to_code = input()
-
-# reader = csv.DictReader(open(sys.argv[1]))
-# print(reader.next())
-
-
-# fieldname = sys.argv[2]
-# new_fieldnames = sys.argv[3:]
-# entries = [e for e in reader]
-# out_entries = []
-# for e in entries:
-#     print(e[fieldname])
-#     for f in new_fieldnames:
-#         print("old value: " + e[f])
-#         e.update({str(f) : input(str(f))})
-#     out_entries.append(e)
-# outfile = open(sys.argv[1] + '_out', 'w')
-# writer = csv.DictWriter() # this is not going to work because we don't write the headers
-# writer.write(out_entries)
-# outfile.close()
+def select_fnames():
+    print("coding "  + str(infile.split("/")[-1:]))
+    print("which fields do you want to code?")
+    for key, value in fname_ids:
+        print((key, value))
+    print("separate by comma")
+fieldnames_to_code = [int(n) for n in input().split(",")]
+print("Coding: ")
+fieldnames_to_code = select_fnames()
+print(str(fieldnames_to_code))
+for f in fieldnames_to_code:
+    print(str(f), fname_ids[f])
